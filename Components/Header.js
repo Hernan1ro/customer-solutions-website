939d744c5c782ad562f204f27d96c2dd23c9e7c5
Hernán D. Mercado <hernandmf@gmail.com>
2022-06-25 14:31:47 -0500

    creating the header component

--- Components/Header.js
+++ Components/Header.js
@@ -1,83 +1,18 @@
-import { useState } from "react";
-import styles from "../styles/layout/Layout.module.css";
-import Link from "next/link";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import styles from "../styles/components/header.module.css";
 
 export const Header = () => {
-  const [open, setOpen] = useState(false);
-  const { pathname } = useRouter();
-
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
   return (
-    <header>
-      <div className={styles.header}>
-        <Link href="/">
-          <a>
-            <Image
-              width={220}
-              height={70}
-              src={"/assets/icons/logo-eslogan.png"}
-              alt="Costumer Solutions"
-              priority
-            />
-          </a>
-        </Link>
-        <div onClick={handleClick}>
-          {!open && (
-            <Image
-              width={40}
-              height={40}
-              src={"/assets/icons/menu.svg"}
-              alt="menu-logo"
-              layout="fixed"
-            />
-          )}
-        </div>
-        <nav className={open ? styles.nav : ""}>
-          <ul>
-            <li className={pathname === "/inicio" ? styles.nav_active : null}>
-              <Link href="/inicio">
-                <a>Soluciones</a>
-              </Link>
-            </li>
-            <fli
-              className={pathname === "/servicios" ? styles.nav_active : null}
-            >
-              <Link href="/servicios">
-                <a>Servicios</a>
-              </Link>
-            </fli>
-            <li className={pathname === "/contácto" ? styles.nav_active : null}>
-              <Link href="/precios">
-                <a>Contácto</a>
-              </Link>
-            </li>
-            <li
-              className={
-                pathname === "/evaluaciondemadurez" ? styles.nav_active : null
-              }
-            >
-              <Link href="/evaluaciondemadurez">
-                <a>Evaluación de Madurez</a>
-              </Link>
-            </li>
-            <div>
-              <Image
-                onClick={handleClick}
-                width={40}
-                height={40}
-                src={"/assets/icons/close.svg"}
-                alt="close-logo"
-                layout="fixed"
-              />
-            </div>
-          </ul>
-        </nav>
-      </div>
+    <header className={styles.header}>
+      <a href="">
+        <img src="./assets/brandlogo/logo.webp" alt="" />
+      </a>
+      <nav>
+        <a href="">Soluciones</a>
+        <a href="">Servicios</a>
+        <a href="">Contácto</a>
+        <a href="">Evaluación de madurez</a>
+        <button>Inicia sesión</button>
+      </nav>
     </header>
   );
 };
