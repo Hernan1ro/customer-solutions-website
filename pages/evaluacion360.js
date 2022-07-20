6e2165716b324237bacc72c61fd6f093bfa4fa47
Hernán D. Mercado <hernandmf@gmail.com>
2022-07-20 12:20:52 -0500

    styling evaluation360 page

--- pages/evaluacion360.js
+++ pages/evaluacion360.js
@@ -1,11 +1,19 @@
 import { Layout } from "../Layout/Layout";
+import styles from "../styles/pages/evaluacion360.module.css";
+import Link from "next/link"
 
 export default function Evaluacion360() {
   return (
     <Layout page="Inicio">
       <section className={styles.container}>
-        <img src="./assets/evaluacion360/heading.jpg" alt="evaluación 360" />
         <div>
+          <img
+            className={styles.hero_img}
+            src="./assets/evaluacion360/heading.jpg"
+            alt="evaluación 360"
+          />
+        </div>
+        <div className={styles.hero_text}>
           <h2>
             Evaluación de nivel de madurez en experiencia del cliente SX360°
           </h2>
@@ -14,12 +22,29 @@ export default function Evaluacion360() {
             grado de digitalización de tu empresa y los pasos a seguir para
             mejorar tu situación.
           </p>
-          <div>
+          <div className={styles.bulletpoint_container}>
             <div>
               <img src="./assets/icons/check.svg" alt="check" />
               <span>30 variables</span>
             </div>
+            <div>
+              <img src="./assets/icons/check.svg" alt="check" />
+              <span>4 dimensiones</span>
+            </div>
+            <div>
+              <img src="./assets/icons/check.svg" alt="check" />
+              <span>Index especifico</span>
+            </div>
+            <div>
+              <img src="./assets/icons/check.svg" alt="check" />
+              <span>Perfil de madurez</span>
+            </div>
           </div>
+          <Link href="/">
+
+          <button>Iniciar test</button>
+          </Link>
+          <small>Tiempo estimado en realizar el test: 5 minutos</small>
         </div>
       </section>
     </Layout>
