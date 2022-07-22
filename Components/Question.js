e6e1fc0541a2140a9b2f47771ee306bff9e035e8
Hernán D. Mercado <hernandmf@gmail.com>
2022-07-22 01:06:26 -0500

    styling inputs from test page

--- Components/Question.js
+++ Components/Question.js
@@ -5,7 +5,11 @@ export const Question = ({ question, answers, inputNumber, handleChange }) => {
   return (
     <div className={styles.question}>
       <p className="question">{`${inputNumber}. ${question}`}</p>
-      <div className={styles.form_answer_container}>
+      <div
+        className={`${styles.form_answer_container} ${
+          answers.length > 4 ? styles.grid : null
+        }`}
+      >
         {answers.map((option) => {
           return (
             <QuestionInput
