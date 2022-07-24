a26da4696f083c55fc29e16f8396db4321cf4576
Hernán D. Mercado <hernandmf@gmail.com>
2022-07-24 11:11:22 -0500

    adding test360 questions

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -69,17 +69,246 @@ export const customerQuestion = [
 export const test360Questions = [
   {
     id: 1,
-    question: "¿Tu eres?",
-    answers: ["Emprendedor", "Empleado", "Comerciante informal"],
+    question:
+      "¿Existe una estrategia de experiencia y satisfaccion al cliente en la empresa?",
+    answers: [
+      "No existe",
+      "Se esta planteando la posibilidad de diseñarla",
+      "Existen acciones puntuales pero sin estrategia",
+      "Mi negocio tiene una estrategia clara y estructurada ",
+    ],
   },
   {
     id: 2,
-    question: "Número de trabajadores",
+    question:
+      "¿Conoces la importancia que la experiencia y satisfacción del cliente pueder tener sobre su negocio?",
     answers: [
-      "Solo 1",
-      "De 1 a 10 empleados",
-      "De 11 a 49 empleados",
-      "Más de 50 empleados",
+      "No creo que aporte grandes beneficios",
+      "Creo que puede aportar algunos beneficios",
+      "Es importante y ya tengo planteado algunos objetivos",
+      "Tengo objetivos concretos y dedico tiempo y recursos para ello",
+      "Mi negocio esta completamente alineado a la experiencia y satisfacción de mi cliente",
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "¿La estrategia de experiencia y satisfacción del cliente es conocida por toda la empresa?",
+    answers: [
+      "No existe estrategia",
+      "Solo la conocen el personal de mercadeo y atencion al cliente",
+      "Solo la conocen el personal de atencion al cliente",
+      "La conocen gran parte de la empresa",
+      "Es conocida por toda la organización",
+    ],
+  },
+  {
+    id: 4,
+    question:
+      "¿Las decisiones de negocio se toman usando información y conocimiento del cliente?",
+    answers: [
+      "Nunca",
+      "Rara vez",
+      "En ocasiones puntuales",
+      "Con mucha frecuencia",
+      "Siempre",
+    ],
+  },
+  {
+    id: 5,
+    question:
+      "¿La empresa mide el impacto economico de la experiencia de cliente?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 6,
+    question:
+      "¿Que importancia  tiene la retroalimentación de tu cliente en tu negocio?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 7,
+    question:
+      "¿Existen suficientes canales de contacto/ interacción disponibles para que los clientes hagan preguntas, reclamos o sugerencias?",
+    answers: [
+      "No existen",
+      "Existen pero no son suficientes",
+      "Existen pero solo para sugerencias y preguntas",
+      "Existen pero no estan totalmente disponibles",
+      "Existen suficientes y totalmente disponibles",
+    ],
+  },
+  {
+    id: 8,
+    question:
+      "¿Cuando la empresa promete hacer algo en cierto tiempo, lo cumple?",
+    answers: ["Nunca", "Casi nunca", "A veces", "Casi siempre", "Siempre"],
+  },
+  {
+    id: 9,
+    question:
+      "¿Utilizas canales digitales para relacionarte  con sus clientes?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 10,
+    question:
+      "¿Mides la satisfaccion y experiencia de tus clientes sistematica y frecuentemente?",
+    answers: ["Nunca", "Casi nunca", "Casi siempre", "Siempre"],
+  },
+  {
+    id: 11,
+    question:
+      "¿Tienes protocolos/procedimientos de servicio y atencion de sus clientes acordes a sus necesidades?",
+    answers: [
+      "No tengo",
+      "Se esta plantenado la posibilidad de tenerlos",
+      "Tengo pero no acordes a las necesidades ",
+      "Tengo y esta totalmente acordes a las necesidades de los clientes",
+    ],
+  },
+  {
+    id: 12,
+    question:
+      "¿Recolectas y utilizas la información de tu cliente para ofrecerle experiencias inovidables?",
+    answers: [
+      "No recojo información de mi cliente",
+      "Recolecto poca información de mi cliente",
+      "Recolecto información pero no la utilizo",
+      "Recolecto y utilizo la información de mis clientes",
+    ],
+  },
+  {
+    id: 13,
+    question:
+      "¿Tus empleados conocen la estrategia de experiencia y satisfaccion de la empresa?",
+    answers: [
+      "No tienen cononcimiento",
+      "Muy pocos empleados",
+      "Algunos empleados",
+      "La gran mayoria de empleados",
+      "Todos los empleados",
+    ],
+  },
+  {
+    id: 14,
+    question:
+      "¿Existe conciencia en todos los empleados de la importancia del cliente para la empresa?",
+    answers: [
+      "No existe conciencia",
+      "Existe en pocos empleados",
+      "Existe en la gran mayoría de empleados",
+      "Existe en todos los empleados",
+    ],
+  },
+  {
+    id: 15,
+    question: "¿La empresa conoce el nivel de satisfacción de sus empleados?",
+    answers: [
+      "No tinen conocimiento",
+      "Muy pocos empleados",
+      "Algunos empleados",
+      "La gran mayoría de empleados",
+      "Todos los empleados",
+    ],
+  },
+  {
+    id: 16,
+    question:
+      "¿Existen programas de capacitación y formación a los empleados  alineados con la estrategia de experiencia del cliente?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 17,
+    question:
+      "¿En que grado los empleados ven a otros empleados como clientes?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 18,
+    question:
+      "¿Que grado de motivación tienen los empleados en su organización frente a la satisfaccion de sus clientes?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 19,
+    question:
+      "¿Existen programas de reconocimiento a los empleados relacionado con la experiencia y satisfacción del cliente?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 20,
+    question:
+      "¿los empleados siempre estan enterados de nuevas promociones, lanzamientos, descuentos o novedades antes de publicarlas a los clientes?",
+    answers: [
+      "Nunca estan enterados",
+      "Rara vez están enterados",
+      "Con frecuencia están enterados",
+      "Siempre están enterados, existe armonia y alineación entre las areas",
+    ],
+  },
+  {
+    id: 21,
+    question:
+      "¿Conoces el grado de recomendación que tus empleados hacen de tu empresa?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 22,
+    question:
+      "¿La empresa cuenta con arquetipos o perfiles de sus clientes ideales?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 23,
+    question:
+      "¿Conoces los momentos sensibles y criticos de tus clientes en tu empresa?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 24,
+    question:
+      "¿Los canales de contacto son de claro conocimiento para tus clientes?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 25,
+    question: "¿Que grado de satisfaccion tienen los clientes en su empresa?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 26,
+    question:
+      "¿Conoces el grado de recomendación que tus clientes hacen de tu empresa?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 27,
+    question:
+      "¿Los canales de contacto estan diseñados según las necesidades de sus clientes?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 28,
+    question:
+      "¿Cuál es el grado de claridad, sencillez y exactitud de las respuestas que se entregan a los clientes?",
+    answers: ["1", "2", "3", "4", "5"],
+  },
+  {
+    id: 29,
+    question:
+      "¿Los clientes viven experiencias enriquecedoras y memorables en su empresa?",
+    answers: ["Si", "No"],
+  },
+  {
+    id: 30,
+    question: "¿Prestas un servicio bien a la primera vez?",
+    answers: [
+      "Nunca",
+      "Pocas veces",
+      "Algunas veces",
+      "Con mucha frecuencia",
+      "Siempre, es mi filosofía",
     ],
   },
 ];
