80987caec39a8dd8fb48caf4c1cc33f736e0d96a
Hernán D. Mercado <hernandmf@gmail.com>
2022-08-04 19:31:16 -0500

    creating a db for informa test

--- pages/api/questions.js
+++ pages/api/questions.js
@@ -722,3 +722,439 @@ export const test360Questions = [
     dimension: "customers",
   },
 ];
+export const test360QuestionsInformal = [
+  {
+    id: 1,
+    question:
+      "¿Existe una estrategia de experiencia y satisfaccion al cliente en la empresa?",
+    options: [
+      { answer: "No existe", points: 2.5 },
+      { answer: "Se esta planteando la posibilidad de diseñarla", points: 5 },
+      { answer: "Existen acciones puntuales pero sin estrategia", points: 7.5 },
+      {
+        answer: "Mi negocio tiene una estrategia clara y estructurada ",
+        points: 10,
+      },
+    ],
+    dimension: "strategy",
+  },
+  {
+    id: 2,
+    question:
+      "¿Conoces la importancia que la experiencia y satisfacción del cliente pueder tener sobre su negocio?",
+    options: [
+      { answer: "No creo que aporte grandes beneficios", points: 2 },
+      { answer: "Creo que puede aportar algunos beneficios", points: 4 },
+      {
+        answer: "Es importante y ya tengo planteado algunos objetivos",
+        points: 6,
+      },
+      {
+        answer:
+          "Tengo objetivos concretos y dedico tiempo y recursos para ello",
+        points: 8,
+      },
+      {
+        answer:
+          "Mi negocio esta completamente alineado a la experiencia y satisfacción de mi cliente",
+        points: 10,
+      },
+    ],
+    dimension: "strategy",
+  },
+  {
+    id: 3,
+    question:
+      "¿Las decisiones de negocio se toman usando información y conocimiento del cliente?",
+    options: [
+      { answer: "Nunca", points: 2 },
+      {
+        answer: "Rara vez",
+        points: 4,
+      },
+      {
+        answer: "En ocasiones puntuales",
+        points: 6,
+      },
+      {
+        answer: "Con mucha frecuencia",
+        points: 8,
+      },
+      {
+        answer: "Siempre",
+        points: 10,
+      },
+    ],
+    dimension: "strategy",
+  },
+
+  {
+    id: 4,
+    question:
+      "¿Que importancia  tiene la retroalimentación de tu cliente en tu negocio?",
+    options: [
+      { answer: "1", points: 2 },
+      {
+        answer: "2",
+        points: 4,
+      },
+      {
+        answer: "3",
+        points: 6,
+      },
+      {
+        answer: "4",
+        points: 8,
+      },
+      {
+        answer: "5",
+        points: 10,
+      },
+    ],
+    dimension: "strategy",
+  },
+  {
+    id: 5,
+    question:
+      "¿Cuando la empresa promete hacer algo en cierto tiempo, lo cumple?",
+    options: [
+      { answer: "Nunca", points: 2 },
+      {
+        answer: "Casi nunca",
+        points: 4,
+      },
+      {
+        answer: "A veces",
+        points: 6,
+      },
+      {
+        answer: "Casi siempre",
+        points: 8,
+      },
+      {
+        answer: "Siempre",
+        points: 10,
+      },
+    ],
+    dimension: "process",
+  },
+  {
+    id: 6,
+    question:
+      "¿Utilizas canales digitales para relacionarte  con sus clientes?",
+    options: [
+      {
+        answer: "Si",
+        points: 10,
+      },
+      {
+        answer: "No",
+        points: 0,
+      },
+    ],
+    dimension: "process",
+  },
+  {
+    id: 7,
+    question:
+      "¿Mides la satisfaccion y experiencia de tus clientes sistematica y frecuentemente?",
+    options: [
+      { answer: "Nunca", points: 2 },
+      {
+        answer: "Casi nunca",
+        points: 4,
+      },
+      {
+        answer: "A veces",
+        points: 6,
+      },
+      {
+        answer: "Casi siempre",
+        points: 8,
+      },
+      {
+        answer: "Siempre",
+        points: 10,
+      },
+    ],
+    dimension: "process",
+  },
+  {
+    id: 8,
+    question:
+      "¿Recolectas y utilizas la información de tu cliente para ofrecerle experiencias inovidables?",
+    options: [
+      { answer: "No recojo información de mi cliente", points: 2.5 },
+      {
+        answer: "Recolecto poca información de mi cliente",
+        points: 5,
+      },
+      {
+        answer: "Recolecto información pero no la utilizo",
+        points: 7.5,
+      },
+      {
+        answer: "Recolecto y utilizo la información de mis clientes",
+        points: 10,
+      },
+    ],
+    dimension: "process",
+  },
+  {
+    id: 9,
+    question:
+      "¿Tus empleados conocen la estrategia de experiencia y satisfaccion de la empresa?",
+    options: [
+      { answer: "No tienen cononcimiento", points: 2 },
+      {
+        answer: "Muy pocos empleados",
+        points: 4,
+      },
+      {
+        answer: "Algunos empleados",
+        points: 6,
+      },
+      {
+        answer: "La gran mayoria de empleados",
+        points: 8,
+      },
+      {
+        answer: "Todos los empleados",
+        points: 10,
+      },
+    ],
+    dimension: "people",
+  },
+  {
+    id: 10,
+    question:
+      "¿Existe conciencia en todos los empleados de la importancia del cliente para la empresa?",
+    options: [
+      { answer: "No existe conciencia", points: 2.5 },
+      {
+        answer: "Existe en pocos empleados",
+        points: 5,
+      },
+      {
+        answer: "Existe en la gran mayoría de empleados",
+        points: 7.5,
+      },
+      {
+        answer: "Existe en todos los empleados",
+        points: 10,
+      },
+    ],
+    dimension: "people",
+  },
+  {
+    id: 11,
+    question: "¿La empresa conoce el nivel de satisfacción de sus empleados?",
+    options: [
+      { answer: "No tienen cononcimiento", points: 2 },
+      {
+        answer: "Muy pocos empleados",
+        points: 4,
+      },
+      {
+        answer: "Algunos empleados",
+        points: 6,
+      },
+      {
+        answer: "La gran mayoria de empleados",
+        points: 8,
+      },
+      {
+        answer: "Todos los empleados",
+        points: 10,
+      },
+    ],
+    dimension: "people",
+  },
+  {
+    id: 12,
+    question:
+      "¿En que grado los empleados ven a otros empleados como clientes?",
+    options: [
+      { answer: "1", points: 2 },
+      {
+        answer: "2",
+        points: 4,
+      },
+      {
+        answer: "3",
+        points: 6,
+      },
+      {
+        answer: "4",
+        points: 8,
+      },
+      {
+        answer: "5",
+        points: 10,
+      },
+    ],
+    dimension: "people",
+  },
+  {
+    id: 13,
+    question:
+      "¿Existen programas de reconocimiento a los empleados relacionado con la experiencia y satisfacción del cliente?",
+    options: [
+      {
+        answer: "Si",
+        points: 10,
+      },
+      {
+        answer: "No",
+        points: 0,
+      },
+    ],
+    dimension: "people",
+  },
+  {
+    id: 14,
+    question:
+      "¿Conoces el grado de recomendación que tus empleados hacen de tu empresa?",
+    options: [
+      {
+        answer: "Si",
+        points: 10,
+      },
+      {
+        answer: "No",
+        points: 0,
+      },
+    ],
+    dimension: "people",
+  },
+  {
+    id: 15,
+    question: "¿Que grado de satisfaccion tienen los clientes en su empresa?",
+    options: [
+      { answer: "1", points: 2 },
+      {
+        answer: "2",
+        points: 4,
+      },
+      {
+        answer: "3",
+        points: 6,
+      },
+      {
+        answer: "4",
+        points: 8,
+      },
+      {
+        answer: "5",
+        points: 10,
+      },
+    ],
+    dimension: "customers",
+  },
+  {
+    id: 16,
+    question:
+      "¿Conoces el grado de recomendación que tus clientes hacen de tu empresa?",
+    options: [
+      {
+        answer: "Si",
+        points: 10,
+      },
+      {
+        answer: "No",
+        points: 0,
+      },
+    ],
+    dimension: "customers",
+  },
+  {
+    id: 17,
+    question:
+      "¿Los canales de contacto estan diseñados según las necesidades de sus clientes?",
+    options: [
+      { answer: "1", points: 2 },
+      {
+        answer: "2",
+        points: 4,
+      },
+      {
+        answer: "3",
+        points: 6,
+      },
+      {
+        answer: "4",
+        points: 8,
+      },
+      {
+        answer: "5",
+        points: 10,
+      },
+    ],
+    dimension: "customers",
+  },
+  {
+    id: 18,
+    question:
+      "¿Cuál es el grado de claridad, sencillez y exactitud de las respuestas que se entregan a los clientes?",
+    options: [
+      { answer: "1", points: 2 },
+      {
+        answer: "2",
+        points: 4,
+      },
+      {
+        answer: "3",
+        points: 6,
+      },
+      {
+        answer: "4",
+        points: 8,
+      },
+      {
+        answer: "5",
+        points: 10,
+      },
+    ],
+    dimension: "customers",
+  },
+  {
+    id: 19,
+    question:
+      "¿Los clientes viven experiencias enriquecedoras y memorables en su empresa?",
+    options: [
+      {
+        answer: "Si",
+        points: 10,
+      },
+      {
+        answer: "No",
+        points: 0,
+      },
+    ],
+    dimension: "customers",
+  },
+  {
+    id: 20,
+    question: "¿Prestas un servicio bien a la primera vez?",
+    options: [
+      { answer: "Nunca", points: 2 },
+      {
+        answer: "Pocas veces",
+        points: 4,
+      },
+      {
+        answer: "Algunas veces",
+        points: 6,
+      },
+      {
+        answer: "Con mucha frecuencia",
+        points: 8,
+      },
+      {
+        answer: "Siempre, es mi filosofía",
+        points: 10,
+      },
+    ],
+    dimension: "customers",
+  },
+];
