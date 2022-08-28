7f6b401e6623386a86f5ceef57e4934387b360c2
Hernán D. Mercado <hernandmf@gmail.com>
2022-08-27 22:53:17 -0500

    connectiong to the database with mysql

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  host: process.env.NEXT_PUBLIC_HOST || "",
-  database: process.env.NEXT_PUBLIC_DATABASE || "",
-  user: process.env.NEXT_PUBLIC_USER || "",
-  password: process.env.NEXT_PUBLIC_PASSWORD || "",
+  host: process.env.NEXT_PUBLIC_HOST || "127.0.0.1",
+  database: process.env.NEXT_PUBLIC_DATABASE || "customer-solutions-db",
+  user: process.env.NEXT_PUBLIC_USER || "root",
+  password: process.env.NEXT_PUBLIC_PASSWORD || "password",
 };
--- backend/src/controllers/lenguage.controller.js
+++ backend/src/controllers/lenguage.controller.js
@@ -1,5 +1,14 @@
-const getUsers = (req, res) => {
-  res.json("Usuarios de la Apí");
+const { getConnection } = require("./../database/database.js");
+
+const getUsers = async (req, res) => {
+  const connection = await getConnection();
+  const result = await connection.query(
+    "SELECT * FROM `customer-solutions-db`.user_db;"
+  );
+
+  console.log(result);
+
+  res.json(result);
 };
 
 module.exports = {
