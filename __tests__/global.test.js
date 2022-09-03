4bd893fac123455811baac50ea3d03da84f1f681
Hernán D. Mercado <hernandmf@gmail.com>
2022-09-02 20:29:26 -0500

    tesint async functions

--- __tests__/global.test.js
+++ __tests__/global.test.js
@@ -25,3 +25,28 @@ test("Probar un callback", () => {
     expect(str).toBe("aloH");
   });
 });
+
+const reverseString2 = (str) => {
+  return new Promise((resolve, reject) => {
+    if (!str) {
+      reject(Error("Error"));
+    }
+    resolve(str.split("").reverse().join(""));
+  });
+};
+
+test("Probar una promesa", () => {
+  return reverseString2("Hola").then((str) => {
+    expect(str).toBe("aloH");
+  });
+});
+
+test("Probar async/await", async () => {
+  const string = await reverseString2("hola");
+  expect(string).toBe("aloh");
+});
+
+afterEach(() => console.log("Después de cada prueba"));
+afterAll(() => console.log("Después de todas las pruebas"));
+beforeEach(() => console.log("Antes de cada prueba"));
+beforeAll(() => console.log("Antes de todas las pruebas"));
