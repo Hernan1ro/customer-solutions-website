ac6c50f0b6a89c4f34412ee8aa92456435403c25
Hernán D. Mercado <hernandmf@gmail.com>
2022-10-08 16:20:26 -0500

    creating a switch to change language

--- Components/Header.js
+++ Components/Header.js
@@ -7,11 +7,28 @@ import { useRouter } from "next/router";
 
 export const Header = ({ hide }) => {
   const [show, setShow] = useState(false);
+  const [openLan, setOpenLang] = useState(false);
   const { locale, asPath, defaultLocale } = useRouter();
 
   const handleClick = () => {
     setShow(!show);
   };
+
+  const localFlag = {
+    "en-US": {
+      url: "/assets/icons/en.jpg",
+    },
+    "es-ES": {
+      url: "/assets/icons/es.jpg",
+    },
+  };
+
+  const { url } = localFlag[locale];
+
+  const handleOpenLan = () => {
+    setOpenLang(!openLan);
+  };
+
   return (
     <>
       <header className={styles.header}>
@@ -47,23 +64,47 @@ export const Header = ({ hide }) => {
                 />
               </>
             ) : null}
-            <div className={styles.language_dropdown}>
-              <img src="/assets/icons/en.jpg" alt="en-ES" />
-              <img src="/assets/icons/arrow1_primary.svg" alt="Arrow" />
-              {/* <Link
-                activeClassName={locale === "es-ES"}
-                href={asPath}
-                locale="es-ES"
+            <div
+              className={`${styles.language_dropdown} ${
+                openLan ? styles.on_click : styles.on_close
+              }`}
+              onClick={handleOpenLan}
+            >
+              <img src={url} alt={locale} />
+              <img
+                className={styles.arrow}
+                src="/assets/icons/arrow1_primary.svg"
+                alt="Arrow"
+              />
+              <div
+                className={`${styles.container_dropdown} ${
+                  openLan ? styles.flex : styles.none
+                }`}
               >
-                es-ES
-              </Link>
-              <Link
-                activeClassName={locale === "en-US"}
-                href={asPath}
-                locale="en-US"
-              >
-                en-US
-              </Link> */}
+                {locale === "en-US" ? (
+                  <Link
+                    activeClassName={locale === "es-ES"}
+                    href={asPath}
+                    locale="es-ES"
+                  >
+                    <div className={styles.lan_link}>
+                      <img src="/assets/icons/es.jpg" alt={locale} />
+                      <p>Es</p>
+                    </div>
+                  </Link>
+                ) : (
+                  <Link
+                    activeClassName={locale === "en-US"}
+                    href={asPath}
+                    locale="en-US"
+                  >
+                    <div className={styles.lan_link}>
+                      <img src="/assets/icons/en.jpg" alt={locale} />
+                      <p>En</p>
+                    </div>
+                  </Link>
+                )}
+              </div>
             </div>
           </div>
         </div>
