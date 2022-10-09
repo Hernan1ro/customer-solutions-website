8f3deb5794cc91808fd7ad2a489ccdca33a28c7d
Hernán D. Mercado <hernandmf@gmail.com>
2022-10-08 20:26:32 -0500

    translating hero services section

--- containers/HeroServices.js
+++ containers/HeroServices.js
@@ -1,14 +1,27 @@
 import styles from "../styles/pages/servicios.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export const HeroServices = () => {
+  const { locale } = useRouter();
+
+  const heroText = {
+    "en-US": {
+      h1: "We help our clients to manage their projects",
+      btn: "Let's have a meeting",
+    },
+    "es-ES": {
+      h1: "Ayudamos a nuestros clientes a gestionar sus proyectos",
+      btn: "Programemos una llamada",
+    },
+  };
+
+  const { h1, btn } = heroText[locale];
   return (
     <section className={styles.servicios}>
-      <h1>
-        Ayudamos a nuestros clientes <br /> a gestionar sus proyectos
-      </h1>
+      <h1>{h1}</h1>
       <Link href="/contacto">
-        <button>Programemos una llamada</button>
+        <button>{btn}</button>
       </Link>
     </section>
   );
